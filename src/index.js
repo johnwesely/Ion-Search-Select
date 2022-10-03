cae685d5ec4443196fcfede047dddf7a2fc006be
John Wesely <john.wesely@tappnetwork.com>
2022-10-03 13:55:36 -0600

    switch to vite

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import IonSearchSelect from './IonSearchSelect.vue';
-import IonSearchSelectModal from './IonSearchSelectModal.vue'
 
 export default {
-    install(Vue, options) {
-        Vue.component('ion-search-select', IonSearchSelect);
-        Vue.component('ion-search-select-modal', IonSearchSelectModal);
+    install(app, options) {
+        app.component('ion-search-select', IonSearchSelect);
     }
 };
