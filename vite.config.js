c9466df726011c67da3fdc17404e9fa61dafac4d
John Wesely <john.wesely@tappnetwork.com>
2022-10-04 12:24:54 -0600

    config updates

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.js"),
-      name: "search-select",
+      name: "ion-search-select",
       filename: (format) => `${format}.js`,
     },
     rollupOptions: {
